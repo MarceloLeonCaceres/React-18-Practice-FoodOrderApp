0a0f57b78038f920cd40d6e03b2f370521f39aad
Marcelo León Cáceres <marcelo.leon.c@gmail.com>
2024-01-08 12:56:27 -0500

    388 Finishing Touches

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -5,43 +5,85 @@ import { currencyFormatter } from "../util/formatting";
 import Input from "./UI/Input.jsx";
 import UserProgressContext from "../store/UserProgressContext.jsx";
 import Button from "./UI/Button.jsx";
+import useHttp from "../hooks/useHttp.js";
+import Error from "./Error.jsx";
+
+const requestConfig = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
 
 export default function Checkout() {
   const cartCtx = useContext(CartContext);
   const userProgressCtx = useContext(UserProgressContext);
+
+  const {
+    data,
+    isLoading: isSending,
+    error,
+    sendRequest,
+    clearData
+  } = useHttp("http://localhost:3000/orders", requestConfig);
 
   const cartTotal = cartCtx.items.reduce(
     (totalPrice, item) => totalPrice + item.quantity * item.price,
     0
   );
 
-  function handleClose(){
+  function handleClose() {
     userProgressCtx.hideCheckout();
   }
 
-  function handleSubmit(event){
+  function handleFinish(){
+    userProgressCtx.hideCheckout();
+    cartCtx.clearCart();
+    clearData();
+  }
+
+  function handleSubmit(event) {
     event.preventDefault();
 
     const fd = new FormData(event.target);
     const customerData = Object.fromEntries(fd.entries()); // {email: test@example.com}
 
-    fetch('http://localhost:3000/orders', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
+    sendRequest(
+      JSON.stringify({
         order: {
           items: cartCtx.items,
-          customer: customerData
-        }
+          customer: customerData,
+        },
       })
-    })
+    );
+  }
 
+  let actions = (
+    <>
+      <Button type="button" textOnly onClick={handleClose}>
+        Close
+      </Button>
+      <Button>Submit Order</Button>
+    </>
+  );
+
+  if (isSending) {
+    actions = <span>Sending order data...</span>;
+  }
+
+  if(data && !error){
+    return <Modal open={userProgressCtx.progress === "checkout"} onClose={handleFinish}>
+      <h2>Success!</h2>
+      <p>Tu pedido se envió de forma exitosa</p>
+      <p>We will get back to you with more details via email within the next few minutes.</p>
+      <p className="modal-actions">
+        <Button onClick={handleFinish}>Ok</Button>
+      </p>
+    </Modal>
   }
 
   return (
-    <Modal open={userProgressCtx.progress === 'checkout'} onClose={handleClose} >
+    <Modal open={userProgressCtx.progress === "checkout"} onClose={handleClose}>
       <form onSubmit={handleSubmit}>
         <h2>Checkout</h2>
         <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
@@ -49,13 +91,11 @@ export default function Checkout() {
         <Input label="Email Address" type="email" id="email" />
         <Input label="Street" type="text" id="street" />
         <div className="control-row">
-            <Input label="Postal Code" type="text" id="postal-code" />
-            <Input label="City" type="text" id="city" />
+          <Input label="Postal Code" type="text" id="postal-code" />
+          <Input label="City" type="text" id="city" />
         </div>
-        <p className="modal-actions">
-            <Button type="button" textOnly onClick={handleClose}>Close</Button>
-            <Button>Submit Order</Button>
-        </p>
+        {error && <Error title="Failed to submit the order" message={error} />}
+        <p className="modal-actions">{actions}</p>
       </form>
     </Modal>
   );
--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -3,7 +3,8 @@ import { createContext, useReducer } from "react";
 const CartContext = createContext({
     items: [],
     addItem: (item) => {},
-    removeItem: (id) => {}
+    removeItem: (id) => {},
+    clearCart: () => {}
 });
 
 function cartReducer(state, action){
@@ -46,8 +47,12 @@ function cartReducer(state, action){
             updatedItems[existingCartItemIndex] = updatedItem;
         }
         return { ...state, items: updatedItems};
-
     }
+
+    if(action.type === 'CLEAR_CART'){
+        return { ...state, items: [] };
+    }
+
     return state;
 }
 
@@ -68,10 +73,17 @@ export function CartContextProvider({ children }){
         });
     }
 
+    function clearCart(){
+        dispatchCartAction({
+            type: 'CLEAR_CART',
+        });
+    }
+
     const cartContext = {
         items: cart.items,
         addItem,
-        removeItem
+        removeItem,
+        clearCart
     };    
 
     return <CartContext.Provider value={cartContext}>
